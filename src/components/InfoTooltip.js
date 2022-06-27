e1292bc6ea9600a6afb56ce944abba016014f28c
Roman Kamelianov <r.kamelyanov@yandex.ru>
2022-06-27 11:35:06 +0300

    feat: jsx infoToolTip

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -1,6 +1,14 @@
-function InfoTooltip () {
+function InfoTooltip ({name, isOpen, img, title, onClose}) {
+    const classNamePopup = `popup ${name}-popup ${isOpen ? 'popup_opened' : ''}`
+  
   return (
-    <></>  
+    <div className={classNamePopup}>
+      <div className="infoToolTip">
+        <img src={img} alt={title} className="infoToolTip__image"/>
+        <h2 className="infoToolTip__title">{title}</h2>
+      </div>
+      <button className="popup__button-glose edit-form__button-glose" type="button" onClick={onClose}></button>
+    </div>  
   )
 }
 
